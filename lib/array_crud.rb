4d38a22212bb8b97c7efde23bf759593d96a2771
delightful-source-3050 <joshmrallen@gmail.com>
2020-02-23 05:18:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Bob Hope", "Marilyn Monroe", "Tina Fey", "Jesse Jackson"]
 end
 
 def add_element_to_end_of_array(array, element)
